9265fb63f9df33215e35505ba76f1034081296b6
Sivakumar Reddy Mettukuru <sivakmr469@gmail.com>
2022-03-30 11:40:34 +0800

    removed creds

--- 01-ec2/ec2.tf
+++ 01-ec2/ec2.tf
@@ -1,4 +1,17 @@
 resource "aws_instance" "myec2" {
    ami = "ami-04893cdb768d0f9ee"
    instance_type = "t2.micro"
+
+   tags = {
+    Name = "HelloWorld"
+  }
+}
+
+resource "aws_instance" "myec2-1" {
+   ami = "ami-04893cdb768d0f9ee"
+   instance_type = "t2.micro"
+
+   tags = {
+    Name = "HelloWorld-1"
+  }
 }
